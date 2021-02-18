02d9e8d4d404da5da3d5cd8a1f3320092f4b3f5c
cloverfiled344 <63333021+cloverfiled344@users.noreply.github.com>
2021-02-18 13:11:12 +0600

    I added scrollViewon the photo and zooming

--- AutoApplication/Cells/FullScreenViewCell.swift
+++ AutoApplication/Cells/FullScreenViewCell.swift
@@ -7,13 +7,26 @@
 
 import UIKit
 
-class FullScreenViewCell: UICollectionViewCell {
+class FullScreenViewCell: UICollectionViewCell, UIScrollViewDelegate {
 
+    @IBOutlet weak var scrollView: UIScrollView!
     @IBOutlet weak var photoView: UIImageView!
     
     override func awakeFromNib() {
         super.awakeFromNib()
     
+        scrollView.delegate = self
+        scrollView.minimumZoomScale = 1.0
+        scrollView.maximumZoomScale = 3.5
+        
     }
 
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        return photoView
+    }
+    
+    func scrollViewDidEndZooming(_ scrollView: UIScrollView, with view: UIView?, atScale scale: CGFloat) {
+        scrollView.zoomScale = 1.0
+    }
+    
 }
